adf89cfa207f10060a6dec04a0e58c4c21afdbcd
Carlos Abdalla <abdalla@msdev.com.br>
2017-10-31 19:41:41 -0400

    test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 import assert from 'assert';
-import concatAll from '../index.js';
+import '../index.js';
 
 describe('concatAll', () => {
-    it('multidimensional array should be returned as a single dimensional array', () => {
-        assert.deepEqual([1, 2, 3, [4,5], 6, 7, 8, [9,], [], [10, [11], [12]], 13, 14].concatAll(), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
-    });
+	it('multidimensional array should be returned as a single dimensional array', () => {
+		assert.deepEqual(
+			[1, 2, 3, [4, 5], 6, 7, 8, [9], [], [10, [11], [12]], 13, 14].concatAll(),
+			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+		);
+	});
 });
